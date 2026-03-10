ad6fde9d16c2337d558bcf876bd3b389f59aa84d
xjrstjrwvg-commits <xjrstjrwvg@privaterelay.appleid.com>
2026-03-10 20:01:08 +0900

    app.py

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ SMALL_TO_LARGE = {"ァ": "ア", "ィ": "イ", "ゥ": "ウ", "ェ": "エ", "ォ":
 DAKU_MAP = {"カ":"ガ", "キ":"ギ", "ク":"グ", "ケ":"ゲ", "コ":"ゴ", "サ":"ザ", "シ":"ジ", "ス":"ズ", "セ":"ゼ", "ソ":"ゾ", "タ":"ダ", "チ":"ヂ", "ツ":"ヅ", "テ":"デ", "ト":"ド", "ハ":"バ", "ヒ":"ビ", "フ":"ブ", "ヘ":"ベ", "ホ":"ボ"}
 HANDAKU_MAP = {"ハ":"パ", "ヒ":"ピ", "フ":"プ", "ヘ":"ペ", "ホ":"ポ"}
 
-# 辞書データ
 DICTIONARY_MASTER = {
     "country": ["アイスランド", "アイルランド", "アゼルバイジャン", "アフガニスタン", "アメリカ", "アラブシュチョウコクレンポウ", "アルジェリア", "アルゼンチン", "アルバニア", "アルメニア", "アンゴラ", "アンティグアバーブーダ", "アンドラ", "イエメン", "イギリス", "イスラエル", "イタリア", "イラク", "イラン", "インド", "インドネシア", "ウガンダ", "ウクライナ", "ウズベキスタン", "ウルグアイ", "エクアドル", "エジプト", "エストニア", "エスワティニ", "エチオピア", "エリトリア", "エルサルバドル", "オーストラリア", "オーストリア", "オマーン", "オランダ", "ガーナ", "カーボベルデ", "ガイアナ", "カザフスタン", "カタール", "カナダ", "ガボン", "カメルーン", "ガンビア", "カンボジア", "キタマケドニア", "ギニア", "ギニアビサウ", "キプロス", "キューバ", "ギリシャ", "キリバス", "キルギス", "グアテマラ", "クウェート", "クックショトウ", "グレナダ", "クロアチア", "ケニア", "コートジボワール", "コスタリカ", "コソボ", "コモロ", "コロンビア", "コンゴキョウワコク", "コンゴミンシュキョウワコク", "サウジアラビア", "サモア", "サントメプリンシペ", "ザンビア", "サンマリノ", "シエラレオネ", "ジブチ", "ジャマイカ", "ジョージア", "シリア", "シンガポール", "ジンバブエ", "スイス", "スウェーデン", "スーダン", "スペイン", "スリナム", "スリランカ", "スロバキア", "スロベニア", "セーシェル", "セキドウギニア", "セネガル", "セルビア", "セントクリストファーネービス", "セントビンセントグレナディーンショトウ", "セントルシア", "ソマリア", "ソロモンショトウ", "タイ", "ダイカンミンコク", "タジキスタン", "タンザニア", "チェコ", "チャド", "チュウオウアフリカ", "チュウカジンミンキョウワコク", "チュニジア", "チョウセンミンシュシュギジンミンキョウワコク", "チリ", "ツバル", "デンマーク", "ドイツ", "トーゴ", "ドミニカキョウワコク", "ドミニカコク", "トリニダードトバゴ", "トルクメニスタン", "トルコ", "トンガ", "ナイジェリア", "ナウル", "ナミビア", "ニウエ", "ニカラグア", "ニジェール", "ニホン", "ニュージーランド", "ネパール", "ノルウェー", "バーレーン", "ハイチ", "パキスタン", "バチカンシコク", "パナマ", "バヌアツ", "バハマ", "パプアニューギニア", "パラオ", "パラグアイ", "バルバドス", "ハンガリー", "バングラデシュ", "ヒガシティモール", "フィジー", "フィリピン", "フィンランド", "ブータン", "ブラジル", "フランス", "ブルガリア", "ブルキナファソ", "ブルネイ", "ブルンジ", "ベトナム", "ベナン", "ベネズエラ", "ベラルーシ", "ベリーズ", "ペルー", "ベルギー", "ポーランド", "ボスニアヘルツェゴビナ", "ボツワナ", "ボリビア", "ポルトガル", "ホンジュラス", "マーシャルショトウ", "マダガスカル", "マラウイ", "マリ", "マルタ", "マレーシア", "ミクロネシアレンポウ", "ミナミアフリカキョウワコク", "ミナミスーダン", "ミャンマー", "メキシコ", "モーリシャス", "モーリタニア", "モザンビーク", "モナコ", "モルディブ", "モルドバ", "モロッコ", "モンゴル", "モンテネグロ", "ヨルダン", "ラオス", "ラトビア", "リトアニア", "リビア", "リヒテンシュタイン", "リベリア", "ルーマニア", "ルクセンブルク", "ルワンダ", "レソト", "レバノン", "ロシア"],
-    "capital": ["アクラ", "アシガバット", "アスタナ", "アスマラ", "アスンシオン", "アディスアベバ", "アテネ", "アバルア", "アピア", "アブジャ", "アブダビ", "アムステルダム", "アルジェ", "アロフィ", "アンカラ", "アンタナナリボ", "アンドララベリャ", "アンマン", "イスラマバード", "ウィーン", "ウィントフック", "ウェリントン", "ウランバートル", "エルサレム", "エレバン", "オスロ", "オタワ", "カイロ", "カストリーズ", "カトマンズ", "カブール", "カラカス", "カンパラ", "キーウ", "キガリ", "キシナウ", "ギテガ", "キト", "キャンベラ", "キングスタウン", "キングストン", "キンシャサ", "グアテマラシティ", "クあラルンプール", "クウェート", "コナクリ", "コペンハーゲン", "ザグレブ", "サヌア", "サラエボ", "サンサルバドル", "サンティアゴ", "サントドミンゴ", "サントメ", "サンホセ", "サンマリノ", "ジブチ", "ジャカルタ", "ジュバ", "ジョージタウン", "シンガポール", "スコピエ", "ストックホルム", "スバ", "スリジャヤワルダナプラコッテ", "セントジョージズ", "セントジョンズ", "ソウル", "ソフィア", "ダカール", "タシケント", "ダッカ", "ダぶリン", "ダマスカス", "タラワ", "タリン", "チュニス", "ティラナ", "ディリ", "ティンプー", "テグシガルパ", "テヘラン", "デリー", "トウキョウ", "ドゥシャンベ", "ドーハ", "ドドマ", "トビリシ", "トリポリ", "ナイロビ", "ナッソー", "ニアメ", "ニコシア", "ヌアクショット", "ヌクアロファ", "ネピドー", "バクー", "バグダッド", "バセテール", "パナマシティ", "バチカン", "ハノイ", "ハバナ", "ハボローネ", "バマコ", "パラマリボ", "ハラレ", "パリ", "パリキール", "ハルツーム", "バレッタ", "バンギ", "バンコク", "バンジュール", "バンダルスリブガワン", "ビエンチャン", "ビクトリア", "ビサウ", "ビシュケク", "ピョンヤン", "ビリニュス", "ファドゥーツ", "ブエノスアイレス", "ブカレスト", "ブダペスト", "フナフティ", "プノンペン", "プライア", "ブラザビル", "ブラジリア", "ブラチスラバ", "プラハ", "フリータウン", "プリシュティナ", "ブリッジタウン", "ブリュッセル", "プレトリア", "ベイルート", "ベオグラード", "ペキン", "ヘルシンキ", "ベルモパン", "ベルリン", "ベルン", "ポートオブスペイン", "ポートビラ", "ポートモレスビー", "ポートルイス", "ボゴタ", "ポドゴリツァ", "ホニアラ", "ポルトープランス", "ポルトノボ", "マジュロ", "マスカット", "マセル", "マドリード", "マナーマ", "マナグア", "マニラ", "マプト", "マラボ", "マルキョク", "マレ", "ミンスク", "ムババーネ", "メキシコシティ", "モガディシュ", "モスクワ", "モナコ", "モロニ", "モンテビデオ", "モンロビア", "ヤウンデ", "ヤムスクロ", "ヤレン", "ラパス", "ラバト", "リーブルビル", "リガ", "リスボン", "リマ", "リヤド", "リュブリャナ", "リロングウェ", "ルアンダ", "ルクセンブルク", "ルサカ", "レイキャビク", "ローマ", "ロゾー", "ロメ", "ロンドン", "ワガドゥグー", "ワシントンディーシー", "ワルシャワ", "ンジャメナ"]
+    "capital": ["アクラ", "アシガバット", "アスタナ", "アスマラ", "アスンシオン", "アディスアベバ", "アテネ", "アバルア", "アピア", "アブジャ", "アブダビ", "アムステルダム", "アルジェ", "アロフィ", "アンカラ", "アンタナナリボ", "アンドララベリャ", "アンマン", "イスラマバード", "ウィーン", "ウィントフック", "ウェリントン", "ウランバートル", "エルサレム", "エレバン", "オスロ", "オタワ", "カイロ", "カストリーズ", "カトマンズ", "カブール", "カラカス", "カンパラ", "キーウ", "キガリ", "キシナウ", "ギテガ", "キト", "キャンベラ", "キングスタウン", "キングストン", "キンシャサ", "グアテマラシティ", "クあラルンプール", "クウェート", "コナクリ", "コペンハーゲン", "ザグレブ", "サヌア", "サラエボ", "サンサルバドル", "サンティアゴ", "サントドミンゴ", "サントメ", "サンホセ", "サンマリノ", "ジブチ", "ジャカルタ", "ジュバ", "ジョージタウン", "シンガポール", "スコピエ", "ストックホルム", "スバ", "スリジャヤワルダナプラコッテ", "セントジョージズ", "セントジョンズ", "ソウル", "ソフィア", "ダカール", "タシケント", "ダッカ", "ダぶリン", "ダマスカス", "タらワ", "タリン", "チュニス", "ティラナ", "ディリ", "ティンプー", "テグシガルパ", "テヘラン", "デリー", "トウキョウ", "ドゥシャンベ", "ドーハ", "ドドマ", "トビリシ", "トリポリ", "ナイロビ", "ナッソー", "ニアメ", "ニコシア", "ヌアクショット", "ヌクアロファ", "ネピドー", "バクー", "バグダッド", "バセテール", "パナマシティ", "バチカン", "ハノイ", "ハバナ", "ハボローネ", "バマコ", "パラマリボ", "ハラレ", "パリ", "パリキール", "ハルツーム", "バレッタ", "バンギ", "バンコク", "バンジュール", "バンダルスリブガワン", "ビエンチャン", "ビクトリア", "ビサウ", "ビシュケク", "ピョンヤン", "ビリニュス", "ファドゥーツ", "ブエノスアイレス", "ブカレスト", "ブダペスト", "フナフティ", "プノンペン", "プライア", "ブラザビル", "ブラジリア", "ブラチスラバ", "プラハ", "フリータウン", "プリシュティナ", "ブリッジタウン", "ブリュッセル", "プレトリア", "ベイルート", "ベオグラード", "ペキン", "ヘルシンキ", "ベルモパン", "ベルリン", "ベルン", "ポートオブスペイン", "ポートビラ", "ポートモレスビー", "ポートルイス", "ボゴタ", "ポドゴリツァ", "ホニアラ", "ポルトープランス", "ポルトノボ", "マジュロ", "マスカット", "マセル", "マドリード", "マナーマ", "マナグア", "マニラ", "マプト", "マラボ", "マルキョク", "マレ", "ミンスク", "ムババーネ", "メキシコシティ", "モガディシュ", "モスクワ", "モナコ", "モロニ", "モンテビデオ", "モンロビア", "ヤウンデ", "ヤムスクロ", "ヤレン", "ラパス", "ラバト", "リーブルビル", "リガ", "リスボン", "リマ", "リヤド", "リュブリャナ", "リロングウェ", "ルアンダ", "ルクセンブルク", "ルサカ", "レイキャビク", "ローマ", "ロゾー", "ロメ", "ロンドン", "ワガドゥグー", "ワシントンディーシー", "ワルシャワ", "ンジャメナ"]
 }
 
 def to_katakana(text):
@@ -32,16 +31,19 @@ def get_variants(char, allow_daku, allow_handaku):
     variants = {char}
     if allow_daku:
         for k, v in DAKU_MAP.items():
-            if char == k: variants.add(v); break
-            if char == v: variants.add(k); break
+            if char == k: variants.add(v)
+            if char == v: variants.add(k)
     if allow_handaku:
         for k, v in HANDAKU_MAP.items():
-            if char == k: variants.add(v); break
-            if char == v: variants.add(k); break
+            if char == k: variants.add(v)
+            if char == v: variants.add(k)
     return variants
 
 @app.route('/')
 def index(): return render_template('index.html')
+
+@app.route('/get_dictionary')
+def get_dictionary(): return jsonify(DICTIONARY_MASTER)
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -50,22 +52,33 @@ def search():
     pos_shift = int(d.get('pos_shift', 0))
     use_shift = d.get('use_shift', False)
     ks_val = int(d.get('ks_abs', 1))
+    use_multi_limit = d.get('use_multi_limit', False)
+    target_total_len = d.get('target_total_len')
     allow_daku = d.get('allow_daku', False)
     allow_handaku = d.get('allow_handaku', False)
     unify_small = d.get('unify_small', False)
+    exclude_conjugates = d.get('exclude_conjugates', False)
+    round_trip = d.get('round_trip', False)
     once_constraint = d.get('once_constraint', False)
     
-    # 経由設定解析
+    blocked_words = set(d.get('blocked_words', []))
+    force_words_dict = set(d.get('force_words', []))
+    
     raw_via = to_katakana(d.get('via_pattern', ""))
     via_steps = [p.strip() for p in raw_via.split('→')] if raw_via else []
-    
-    # 必須文字・除外文字
+
     raw_mc = to_katakana(d.get('must_char', ""))
+    mc_counts = {}
+    if use_multi_limit:
+        for item in re.split('[、,]', raw_mc):
+            if ':' in item:
+                char, count = item.split(':')
+                mc_counts[to_katakana(char.strip())] = int(count)
     must_chars = [c for c in re.split('[、,]', raw_mc) if c]
+
     exclude_chars = to_katakana(d.get('exclude_chars', ""))
     ex_list = [c.strip() for c in re.split('[、,]', exclude_chars) if c.strip()]
     
-    # グループ制約
     gc_text = to_katakana(d.get('group_constraints', ""))
     group_constraints = [re.split('[、,]', line) for line in gc_text.split('\n') if line.strip()]
 
@@ -74,25 +87,32 @@ def search():
     end_char = to_katakana(d.get('end_char', ""))
     selected_cats = d.get('categories', ["country"])
     
-    word_pool = []
+    temp_pool = []
     for cat in selected_cats:
         for w in DICTIONARY_MASTER.get(cat, []):
+            if w in blocked_words: continue
             check_w = "".join([SMALL_TO_LARGE.get(c, c) for c in w]) if unify_small else w
             if any(ex in check_w for ex in ex_list): continue
-            word_pool.append(w)
-    word_pool = list(set(word_pool))
+            temp_pool.append(w)
+    
+    if exclude_conjugates:
+        counts = Counter([(get_clean_char(w, "head"), get_clean_char(w, "tail")) for w in temp_pool if w])
+        word_pool = [w for w in temp_pool if counts[(get_clean_char(w, "head"), get_clean_char(w, "tail"))] == 1]
+    else:
+        word_pool = list(set(temp_pool))
 
     head_index = defaultdict(list)
+    tail_index = defaultdict(list)
     for w in word_pool:
         head_index[get_clean_char(w, "head", pos_shift)].append(w)
+        tail_index[get_clean_char(w, "tail", pos_shift)].append(w)
 
     results = []
     start_time = time.time()
 
     def solve(path, current_total_len):
-        if time.time() - start_time > 10 or len(results) >= 1000: return
+        if time.time() - start_time > 15 or len(results) >= 1500: return
         
-        # ステップごとの経由文字チェック
         step_idx = len(path) - 1
         if via_steps and step_idx < len(via_steps):
             v_char = via_steps[step_idx]
@@ -102,38 +122,39 @@ def search():
             if h not in v_vars and t not in v_vars: return
 
         if len(path) == max_len:
-            full_txt = "".join(path)
-            # 必須文字・単一制約
-            if once_constraint:
-                if not all(full_txt.count(mc) == 1 for mc in must_chars): return
-            elif not all(mc in full_txt for mc in must_chars): return
-            
-            # グループ制約
+            if not force_words_dict.issubset(set(path)): return
+            full_current = "".join(path)
+            if unify_small:
+                full_current = "".join([SMALL_TO_LARGE.get(c, c) for c in full_current])
             if group_constraints:
+                path_set = set(path)
                 for group in group_constraints:
-                    if not any(g in set(path) for g in group): return
-            
-            # 終了字判定
+                    if not any(target in path_set for target in group if target): return
+            if use_multi_limit:
+                if not all(full_current.count(c) == n for c, n in mc_counts.items()): return
+            elif must_chars:
+                if once_constraint:
+                    if not all(full_current.count(mc) == 1 for mc in must_chars): return
+                else:
+                    if not all(mc in full_current for mc in must_chars): return
+            if target_total_len is not None and current_total_len != target_total_len: return
             last_tail = get_clean_char(path[-1], "tail", pos_shift)
             allowed_ends = get_variants(end_char, allow_daku, allow_handaku) if end_char else set()
             if end_char and last_tail not in allowed_ends: return
-            
             results.append(list(path))
             return
         
-        src_char = get_clean_char(path[-1], "tail", pos_shift)
-        targets = {src_char}
-        if use_shift:
-            def shift_kana(c, n):
-                if c not in KANA_LIST: return c
-                return KANA_LIST[(KANA_LIST.index(c) + n) % len(KANA_LIST)]
-            targets = {shift_kana(src_char, ks_val), shift_kana(src_char, -ks_val)}
-
+        is_odd = (len(path) % 2 != 0)
+        src_char = get_clean_char(path[-1], "tail" if not round_trip or is_odd else "head", pos_shift)
+        def shift_kana(c, n):
+            if c not in KANA_LIST: return c
+            return KANA_LIST[(KANA_LIST.index(c) + n) % len(KANA_LIST)]
+        targets = {shift_kana(src_char, abs(ks_val)), shift_kana(src_char, -abs(ks_val))} if use_shift and ks_val != 0 else {src_char}
         all_targets = set()
         for bt in targets: all_targets.update(get_variants(bt, allow_daku, allow_handaku))
-
+        idx = head_index if (not round_trip or is_odd) else tail_index
         for tc in all_targets:
-            for nxt in head_index.get(tc, []):
+            for nxt in idx.get(tc, []):
                 if nxt not in path: solve(path + [nxt], current_total_len + len(nxt))
 
     starts = [start_word] if start_word in word_pool else word_pool

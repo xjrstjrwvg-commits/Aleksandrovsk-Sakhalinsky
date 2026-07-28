dbb04ce6d002bd5894d57a7834e7e366a0ed382d
xjrstjrwvg-commits <xjrstjrwvg@privaterelay.appleid.com>
2026-07-28 20:14:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,9 @@ def search():
 
     result = runSearch(params)
     return jsonify(result)
+    # 結果を返す
+    return jsonify(result)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
